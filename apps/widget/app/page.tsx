40fd194b286f51662484f898c6753035580c0168
ecryptoguru <ecryptoguru@gmail.com>
2025-09-04 13:59:47 +0530

    test

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@workspace/ui/components/button"
+import { Input } from "@workspace/ui/components/input"
+import { add } from "@workspace/math/add"
 
 export default function Page() {
   return (
@@ -6,6 +8,8 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello Widget</h1>
         <Button size="sm">Button</Button>
+        <p>{add(3, 3)}</p>
+        <Input />
       </div>
     </div>
   )
