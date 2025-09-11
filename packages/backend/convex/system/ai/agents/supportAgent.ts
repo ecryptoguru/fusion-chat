3777dc31f9d482f15456a928d582ef9d46f5724f
ecryptoguru <ecryptoguru@gmail.com>
2025-09-11 18:22:43 +0530

    Update10a

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,9 +1,9 @@
 import { openai } from "@ai-sdk/openai";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api";
-
+import { SUPPORT_AGENT_PROMPT } from "../constants";
 
 export const supportAgent = new Agent(components.agent, {
   chat: openai.chat("gpt-4o-mini"),
-  instructions: "You are a customer support agent",
+  instructions: SUPPORT_AGENT_PROMPT,
 });
