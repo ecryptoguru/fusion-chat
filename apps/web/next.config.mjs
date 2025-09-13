c2a0260b447b0f5e4872bf86446f307697c4e7b3
ecryptoguru <ecryptoguru@gmail.com>
2025-09-13 19:34:48 +0530

    patch

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
   devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/conversations",
+        permanent: false,
+      },
+    ]
+  }
 }
-
-export default nextConfig
