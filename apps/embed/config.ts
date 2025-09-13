a2e982b50399c2aa5c237885fab45e144dabe088
ecryptoguru <ecryptoguru@gmail.com>
2025-09-13 19:20:31 +0530

    UpdateFinal

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+  WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+  DEFAULT_ORG_ID: "org_32NMknmpQgC8ejZCT9NZyn8kxsl",
+  DEFAULT_POSITION: "bottom-right" as const,
+};
